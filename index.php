4b0fb7a85c73bf5a094bf31353e06f14195bc78d
Hernan Gonzalez <hernan.gonzalez@mdinteractivo.net>
2017-11-10 09:52:03 -0300

    initial Template

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="Alegria">
+
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+    <title>Alegria</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center" style=" top:6px;">
+            <img src="images/Cabezal.png" style="width: 100%" />
+            <div class="col-sm-12 text-center" style="position: absolute; top:6px;">
+                <img src="images/Logo.png">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+            <img src="images/Imagen Principal.png" class="img_main"/>
+            <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:45px;">GET A</div>
+            <div class="col-sm-12 text-center font_main_yellow" style="position: absolute; top:65px;">150%</div>
+            <div class="col-sm-12 text-center font_main_yellow" style="position: absolute; top:105px;">BONUS</div>
+            <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:158px;">UP TO</div>
+            <div class="col-sm-12 text-center" style="position: absolute; top:174px;"><span class="rs">R$</span><span class="font_main_yellow">300</span></div>
+            <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:230px;">ON YOUR 1<sup>o</sup> DEPOSIT</div>
+            <div class="col-sm-12 text-center" style="position: absolute; bottom:0px; padding-right: 57px;">
+                <img src="images/Inmortal Romance.png">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+            <div style="background-color: #00893d; height: 5px;"></div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+            <div style="background-color: #212121; padding-top: 15px">
+                <div class="font_body_white">Spin the reels with this exlusive WELCOME OFFER</div> 
+                <div class="font_body_yellow" style="font-weight:bold;margin-top: 15px">***150% BONUS up to R$300***</div>
+                <div  style="margin-top: 15px">
+                    <span class="font_body_white">1.</span>
+                    <span><a style="color: yellow" href="" class="sign_up"><u>Sign up</u></a></span>
+                    <span class="font_body_white"> for an account</span>
+                </div>
+                <div class="font_body_white" style="margin-top: 15px">2. Deposit R$20 0r more</div>
+                <div class="font_body_white" style="margin-top: 15px">3. Get 150% match up to R$300</div>
+
+                <div class="row text-center">
+                    <button class="continue">CONTINUE</button>
+                </div>
+
+                <div class="row text-center" style="margin-top: 15px">
+                     <a href="" style="color:white; font-size:12px;margin-top: 15px"><u>New customers.T&Cs apply.</u></a> 
+                </div>
+                <div class="row text-center" style="margin-top: 15px; padding-bottom: 10px">
+                    <span style="color:white; font-size:9px;">Privacy Policy</span>
+                    <span style="color:white; font-size:9px;">|</span>
+                    <span style="color:white; font-size:9px;">Terms and Conditions</span>
+                    <span style="color:white; font-size:9px;">|</span>
+                    <span style="color:white; font-size:9px;">Contact us</span>
+                </div>            
+            </div>           
+        </div>            
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <div style="background-color: #00893d; padding-top: 15px; padding-bottom: 5px">
+                <div class="row">
+                    <div class="col-sm-5 col-sm-offset-0 text-center">
+                        <img src="images/Logo.png" style="margin-left: 25px">
+                        <img src="images/Sello oro.png" style="margin-left: 45px">
+                        <img src="images/18mas.png">
+                        <p style="font-size: 9px; color: white; margin-top: 10px; margin-left: -70px;font-family: 'Lato-Bold'">Copyright C 2017 AlegriaBet.com</p>
+                    </div>
+                    <div class="col-sm-5 col-sm-offset-1 text-right" style="padding-right: 0px">
+                        <p style="font-size: 9px; color: white; margin-top: 16px; padding-right: 10px; font-family: 'Lato-Bold'">Alegriabrt.com is operated by EJAD TECH LTD B.V.
+                        with domicille in Dr. MJ Hugenholtzweg z/n UTS. Gebouw,
+                        Curacao, holder curaçao gaming license no 1668/JAZ</p>                      
+                    </div> 
+                    <div class="col-sm-1 text-center" style="padding-left: 0px; margin-top: 20px">
+                        <img src="images/Logo Curacao.png" style="padding-right: 15px">
+                    </div>  
+                </div>
+            </div>           
+        </div>            
+    </div>
+    
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
