2789b9b8568c2f2df05bccf2e5aabb69a1795eb4
leyany <leyany.yera.moya@gmail.com>
2017-11-25 12:57:20 -0300

    last change

--- index.php
+++ index.php
@@ -1,100 +1,141 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta name="description" content="Alegria">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+<title>AlegriaBet.com</title>
+<style>
 
-    <link type="text/css" rel="stylesheet" href="css/style.css"/>
-    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+body { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; margin: 0 !important; padding: 0 !important; width: 100% !important; }
+.continue { margin-top: 15px; width: 170px; border: 1px solid yellow; color: white; background-image: url("images/continue.png"); font-size: 20px; border-radius: 0px 0px 0px 0px; font-family: 'Lato'}
+button.continue:hover { background-image: url('images/continue hover.png'); color: #0b4004;}
 
-    <title>Alegria</title>
+.margen_lateral {width:25%}
+.cuerpo {width: 50%;}
+.borde_lateral { width: 3%; bgcolor: #00933D}
+.cuerpo_interior {width: 94%}
+.contenido { width: 100%; height: auto; }
+.spin_reels {}
+
+@media only screen and (max-width: 599px) {
+  .margen_lateral{width: 0%!important}
+  .cuerpo {width: 100%!important}
+  .continue { margin-top: 15px; width: 170px; border: 1px solid yellow; color: white; background-image: url("images/continue.png"); font-size: 20px; border-radius: 0px 0px 0px 0px; font-family: 'Lato'}
+  button.continue:hover { background-image: url('images/continue hover.png'); color: #0b4004;}
+}
+@media only screen and (max-width: 479px) {
+  .margen_lateral{width: 0%!important}
+  .cuerpo {width: 100%!important}
+  .continue { margin-top: 15px; width: 170px; border: 1px solid yellow; color: white; background-image: url("images/continue.png"); font-size: 20px; border-radius: 0px 0px 0px 0px; font-family: 'Lato'}
+button.continue:hover { background-image: url('images/continue hover.png'); color: #0b4004;}
+}
+</style>
 </head>
-<body>
-    <div class="" style="background-color: #00933D; height: 1000px; width: 750px; margin-left: 20%; position: absolute;">
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <img src="images/Cabezal.png" style="width: 96%;"/>
-                <div class="col-sm-12 text-center" style="position: absolute; top:6px;">
-                    <a href="">
-                        <img src="images/Logo.png">
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <img src="images/Imagen Principal.png" class="img_main"/>
-                <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:45px;">GET A</div>
-                <div class="col-sm-12 text-center font_main_yellow" style="position: absolute; top:65px;">150%</div>
-                <div class="col-sm-12 text-center font_main_yellow" style="position: absolute; top:105px;">BONUS</div>
-                <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:158px;">UP TO</div>
-                <div class="col-sm-12 text-center" style="position: absolute; top:174px;"><span class="rs">R$</span><span class="font_main_yellow">300</span></div>
-                <div class="col-sm-12 text-center font_main_white" style="position: absolute; top:230px;">ON YOUR 1<sup>o</sup> DEPOSIT</div>
-                <div class="col-sm-12 text-center" style="position: absolute; bottom:0px; padding-right: 57px;">
-                    <img src="images/Inmortal Romance.png">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div style="background-color: #00933D; height: 10px;"></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <div style="background-color: #212121; padding-top: 15px; width: 96%; margin-left: 2%;">
-                    <div class="font_body_white">Spin the reels with this exlusive WELCOME OFFER</div> 
-                    <div class="font_body_yellow" style="font-weight:bold;margin-top: 15px">***150% BONUS up to R$300***</div>
-                    <div  style="margin-top: 15px">
-                        <span class="font_body_white">1.</span>
-                        <span><a style="color: yellow" href="" class="sign_up"><u>Sign up</u></a></span>
-                        <span class="font_body_white"> for an account</span>
-                    </div>
-                    <div class="font_body_white" style="margin-top: 15px">2. Deposit R$20 0r more</div>
-                    <div class="font_body_white" style="margin-top: 15px">3. Get 150% match up to R$300</div>
 
-                    <div class="row text-center">
-                        <button class="continue">CONTINUE</button>
-                    </div>
+<body style="margin:0px;" bgcolor="#ffffff">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+  <tr>
+    <td class="margen_lateral"></td>
+    <td class="cuerpo">
+      <div >
+        <img src="images/Cabezal.png" width="100%">
+        <div style="margin-left: 44%">
+          <a href=""><img src="images/Logo.png" width="8%" style="position: absolute; top: 0%;"></a>
+        </div>
+      </div>
+    </td>
+    <td class="margen_lateral"></td>
+  </tr>
 
-                    <div class="row text-center" style="margin-top: 15px">
-                         <a href="" style="color:white; font-size:12px;margin-top: 15px"><u>New customers.T&Cs apply.</u></a> 
-                    </div>
-                    <div class="row text-center" style="margin-top: 15px; padding-bottom: 10px">
-                        <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; ">Privacy Policy</span></a>
-                        <span style="color:white; font-size:9px;">|</span>
-                        <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; text-decoration: none;">Terms and Conditions</span></a>
-                        <span style="color:white; font-size:9px;">|</span>
-                        <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; text-decoration: none;">Contact us</span></a>
-                    </div>            
-                </div>           
-            </div>            
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div style="background-color: #00933D; padding-top: 15px; padding-bottom: 5px">
-                    <div class="row">
-                        <div class="col-sm-5 col-sm-offset-0 text-center">
-                            <a href=""><img src="images/Logo.png" style="margin-left: 25px"></a>
-                            <img src="images/Sello oro.png" style="margin-left: 45px">
-                            <img src="images/18mas.png">
-                            <p style="font-size: 9px; color: white; margin-top: 10px; margin-left: -70px;font-family: 'Lato-Bold'">Copyright C 2017 AlegriaBet.com</p>
-                        </div>
-                        <div class="col-sm-5 col-sm-offset-1 text-right" style="padding-right: 0px">
-                            <p style="font-size: 9px; color: white; margin-top: 16px; padding-right: 10px; font-family: 'Lato-Bold'">Alegriabrt.com is operated by EJAD TECH LTD B.V.
-                            with domicille in Dr. MJ Hugenholtzweg z/n UTS. Gebouw,
-                            Curacao, holder curaçao gaming license no 1668/JAZ</p>                      
-                        </div> 
-                        <div class="col-sm-1 text-center" style="padding-left: 0px; margin-top: 20px">
-                            <a href=""><img src="images/Logo Curacao.png" style="padding-right: 15px"></a>
-                        </div>  
-                    </div>
-                </div>           
-            </div>            
-        </div>
-    </div>
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <tr>
+    <td class="margen_lateral"></td>
+    <td width="50%">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+        <tr>
+          <td width="3%" bgcolor="#00933D" class="borde_lateral"></td>
+          <td width="94%">
+            <a href=""><img src="images/banner.png" width="100%"></a>
+          </td>
+          <td width="3%" bgcolor="#00933D" class="borde_lateral"></td>
+        </tr>
+      </table>
+    </td>
+    <td class="margen_lateral"></td>
+  </tr>
+
+  <tr>
+    <td class="margen_lateral"></td>
+    <td class="cuerpo" height="10px" bgcolor="#00933D"></td>
+    <td class="margen_lateral"></td>
+  </tr>
+
+  <tr>
+    <td class="margen_lateral"></td>
+    <td class="cuerpo">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+        <tr style="padding: 0">
+          <td class="borde_lateral" bgcolor="#00933D"></td>
+          <td width="94%" bgcolor="#212121" class="cuerpo_interior" >
+            <div class="contenido" style="text-align: center">
+              <p class="spin_reels" style="color: white; font-size:24px; font-family: 'Lato';">Spin the reels with this exlusive WELCOME OFFER</p>
+              <p class="bonus" style="color:yellow; font-size:30px; font-family: 'Lato'; font-weight: bold">***150% BONUS up to R$300***</p>
+              <p class="sign" style="color:white; font-size:24px; font-family: 'Lato'; font-weight: bold">1. <a href="" style="color: yellow"><u>Sign up</u></a><span> for an account</p></span>
+              <p class="deposit" style="color:white; font-size:24px; font-family: 'Lato'; font-weight: bold">2. Deposit R$20 or more</p>
+              <p class="get" style="color:white; font-size:24px; font-family: 'Lato'; font-weight: bold">3. Get 150% match up to R$300</p>
+              <button class="continue">CONTINUE</button>
+              <div class="new_customer" style="margin-top: 3%; margin-bottom: 3%">
+                <a href="" style="color:white; font-size:12px;"><u>New customers.T&Cs apply.</u></a>
+              </div>
+              <div class="row text-center" style="margin-top: 15px; padding-bottom: 10px">
+                <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; ">Privacy Policy</span></a>
+                <span style="color:white; font-size:9px;">|</span>
+                <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; text-decoration: none;">Terms and Conditions</span></a>
+                <span style="color:white; font-size:9px;">|</span>
+                <a style="text-decoration: none;" href=""><span style="color:white; font-size:9px; text-decoration: none;">Contact us</span></a>
+              </div>
+            </div>
+          </td>
+          <td class="borde_lateral" bgcolor="#00933D"></td>
+        </tr>
+      </table>
+    </td>
+    <td class="margen_lateral"></td>
+  </tr>
+
+  <tr>
+    <td class="margen_lateral"></td>
+    <td class="cuerpo" height="auto" bgcolor="#00933D">
+      <a href=""><p style="text-align: center; color: white; font-size: 12px; font-family: 'Lato'"><u>UNSUBSCRIBE</u></p></a>
+    </td>
+    <td class="margen_lateral"></td>
+  </tr>
+
+  <tr>
+    <td class="margen_lateral"></td>
+    <td class="cuerpo" height="auto" bgcolor="#00933D">
+      <table>
+        <tr>
+          <td width="30%" style="text-align: center;">
+            <a href=""><img src="images/Logo.png" width="25%" style=""></a>
+            <p style="font-size: 9px; color: white; font-family: 'Lato'">Copyright C 2017 AlegriaBet.com</p>
+          </td>
+          <td width="15%" style="text-align: left;">
+            <img src="images/Sello oro.png">
+            <img src="images/18mas.png" style="padding-bottom: 2%">
+          </td>
+          <td width="40%" style="text-align: right;">
+            <p style="font-size: 9px; color: white; font-family: 'Lato'">Alegriabrt.com is operated by EJAD TECH LTD B.V. with domicille in Dr. MJ Hugenholtzweg z/n UTS. Gebouw, Curacao, holder curaçao gaming license no 1668/JAZ</p>
+          </td>
+          <td width="10%" style="text-align: center;">
+            <a href=""><img src="images/Logo Curacao.png"></a>
+          </td>
+        </tr>
+      </table>
+    </td>
+
+    <td class="margen_lateral"></td>
+  </tr>
+</table>
 </body>
 </html>
